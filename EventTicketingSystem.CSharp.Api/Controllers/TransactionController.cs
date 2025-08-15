e5433db9a836f0f5624ddaba3d79124f36b786d6
Khai Sian Taang <khaisian.khaisiantaang@gmail.com>
2025-08-16 00:30:15 +0630

    refactor: Fix authorize in transactioncontroller

--- EventTicketingSystem.CSharp.Api/Controllers/TransactionController.cs
+++ EventTicketingSystem.CSharp.Api/Controllers/TransactionController.cs
@@ -14,24 +14,28 @@ public class TransactionController : ControllerBase
     }
 
     [HttpGet("GetTicketList/{eventCode}")]
+    [Authorize]
     public async Task<IActionResult> GetTicketTypeList(string eventCode)
     {
         return Ok(await _blTransaction.GetTicketTypeList(eventCode));
     }
 
     [HttpGet("GetTransactionHistoryList")]
+    [Authorize]
     public async Task<IActionResult> GetTransactionHistoryList()
     {
         return Ok(await _blTransaction.GetTransactionHistoryList());
     }
 
     [HttpGet("GetTransactionHistoryDetail/{transactionCode}")]
+    [Authorize]
     public async Task<IActionResult> GetTransactionHistoryDetail(string transactionCode)
     {
         return Ok(await _blTransaction.GetTransactionHistoryDetail(transactionCode));
     }
 
     [HttpPost("ProcessTransaction")]
+    [AllowAnonymous]
     public async Task<IActionResult> ProcessTransaction([FromBody] TransactionRequestModel requestModel)
     {
         return Ok(await _blTransaction.ProcessTransaction(requestModel));
